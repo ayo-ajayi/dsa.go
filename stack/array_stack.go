7a0142630e548f03340dafdbe49ff2c421aedb2c
ayo-ajayi <ajayiayomide247@gmail.com>
2024-06-06 17:40:09 +0200

    comment on error handling in stack/array_stack.go

--- stack/array_stack.go
+++ stack/array_stack.go
@@ -20,8 +20,8 @@ func (s *ArrayStack) Peek() any {
 
 func (s *ArrayStack) Pop() any {
 	if s.IsEmpty() {
-		return nil
-	}
+		return nil 	//think of a better error handling technique
+	}				//stack could contain (any) datatype that is nil...which would lead to confusion
 	pop := (*s)[len(*s)-1]
 	*s = (*s)[:len(*s)-1]
 	return pop
